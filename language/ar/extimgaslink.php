66c7589261427dc131b385c12dd9e3029a87b487
Basil Taha Alhitary <alhitary@users.noreply.github.com>
2015-05-29 16:01:38 +0300

    Create extimgaslink.php

---
+++ language/ar/extimgaslink.php
@@ -0,0 +1,28 @@
+<?php
+/**
+*
+* External Image as link [Arabic]
+*
+* @package language
+* @version 1.0.0
+* @copyright (c) 2015 phpbb.de
+* @license http://opensource.org/licenses/gpl-license.php GNU Public License
+*
+* Translated By : Bassel Taha Alhitary - www.alhitary.net
+*/
+
+/**
+* DO NOT CHANGE
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+	'EXTIMGLINK'	=> '[ صورة خارجية ]',
+));
