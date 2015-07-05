8f28388a02cd7d3b580026e0ca006d20aaf88913
Martin Beckmann <gn#36@phpbb.de>
2015-07-05 15:44:25 +0200

    Add functional tests for travis

---
+++ tests/functional/install_test.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+*
+* @package phpBB.de Pastebin
+* @copyright (c) 2014 phpBB.de
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+namespace phpbbde\externalimgaslink\tests\functional;
+
+/**
+* @group functional
+*/
+class install_test extends \phpbb_functional_test_case
+{
+	static protected function setup_extensions()
+	{
+		return array('phpbbde/externalimgaslink');
+	}
+
+	public function test_validate_externalimgaslink()
+	{
+		$crawler = self::request('GET', 'viewtopic.php?t=1');
+		$this->assertContains('phpBB3', $crawler->filter('h3')->text());
+	}
+}
