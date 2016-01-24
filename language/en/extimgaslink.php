a6d43383de00fabc1cc5df777ecca18a477883e6
Oliver Schramm <oliver.schramm97@gmail.com>
2016-01-24 18:45:54 +0100

    Fix english language (thank you @VSEphpbb :heart:)

--- language/en/extimgaslink.php
+++ language/en/extimgaslink.php
@@ -24,7 +24,7 @@ if (empty($lang) || !is_array($lang))
 
 $lang = array_merge($lang, array(
 	'EXTIMGASLINK_CONFIG'			=> 'Permitted images',
-	'EXTIMGASLINK_CONFIG_EXPLAIN'	=> 'Images, that are available through a secure connection, can be additionally permitted. Any other images are replaced with links. Images, that are available on the same domain as this board, are always permitted.',
+	'EXTIMGASLINK_CONFIG_EXPLAIN'	=> 'You can allow images served from this board’s domain only, or from both this board and external secure websites to be shown. All other images will be replaced with links.',
 	'EXTIMGASLINK_INVALID_CONFIG'	=> 'The image origin you specified is invalid.',
 	'EXTIMGASLINK_SECURE_SITES'		=> 'only images from secure websites and this domain',
 	'EXTIMGASLINK_SERVER_ONLY'		=> 'only images from this domain',
