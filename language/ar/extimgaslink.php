ec29715e8af77b99cf8905b3f55b23649b8d3a12
Basil Taha Alhitary <alhitary@users.noreply.github.com>
2016-07-22 17:50:23 +0300

    Update extimgaslink.php

--- language/ar/extimgaslink.php
+++ language/ar/extimgaslink.php
@@ -4,8 +4,8 @@
 * External Image as link [Arabic]
 *
 * @package language
-* @version 1.0.0
-* @copyright (c) 2015 phpbb.de
+* @version 1.1.0
+* @copyright (c) 2016 phpbb.de
 * @license http://opensource.org/licenses/gpl-license.php GNU Public License
 *
 * Translated By : Bassel Taha Alhitary - www.alhitary.net
@@ -24,5 +24,11 @@ if (empty($lang) || !is_array($lang))
 }
 
 $lang = array_merge($lang, array(
+	'EXTIMGASLINK_CONFIG'			=> 'الصور المسموح بها ',
+	'EXTIMGASLINK_CONFIG_EXPLAIN'	=> 'تستطيع السماح بعرض الصور الموجودة في موقعك فقط , أو من موقعك والمواقع الخارجية الموثوق بها. جميع الصور الأخرى ستظهر على شكل روابط فقط.',
+	'EXTIMGASLINK_INVALID_CONFIG'	=> 'رابط الصورة التي حددتها غير صالح.',
+	'EXTIMGASLINK_SECURE_SITES'		=> 'الصور من المواقع الآمنة وهذا الموقع فقط ',
+	'EXTIMGASLINK_SERVER_ONLY'		=> 'الصور من هذا الموقع فقط ',
+
 	'EXTIMGLINK'	=> '[ صورة خارجية ]',
 ));
